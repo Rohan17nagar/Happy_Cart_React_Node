1a083125dd0100f73c6edfcd728cb319838a4ab2
Rohan Nagar <rohan17nagar@gmail.com>
2021-08-29 18:20:41 +0530

    basic details onchangehandler added

--- frontend/src/components/userRegistration/BasicDetails.js
+++ frontend/src/components/userRegistration/BasicDetails.js
@@ -1,28 +1,73 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import "./BasicDetails.scss";
 
 const BasicDetails = () => {
-  const [username, setUserName] = useState("");
+  //useStates definition
+  const [basicDetails, setBasicDetails] = useState({});
+
+  //useEffects calls
+  useEffect(() => {
+    let userDetails = {
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+      mobile: "",
+    };
+    setBasicDetails(userDetails);
+  }, []);
+
+  // Function Definitions
+  const handleTextFieldChange = (e) => {
+    setBasicDetails({});
+  };
+
   return (
     <div className="bd-container">
-      <h2 className="bd-heading">Enter Details :</h2>
+      <h3 className="bd-heading">Enter Basic Details :</h3>
 
       <label>First Name* :</label>
-      <input type="text" value={username} />
+      <input
+        type="text"
+        onChange={(e) => handleTextFieldChange(e)}
+        value={basicDetails.username}
+      />
 
       <label>Last Name* :</label>
-      <input type="text" value={username} />
+      <input
+        type="text"
+        onChange={(e) => handleTextFieldChange(e)}
+        value={basicDetails.username}
+      />
 
       <label>Email* :</label>
-      <input type="text" value={username} />
+      <input
+        type="text"
+        onChange={(e) => handleTextFieldChange(e)}
+        value={basicDetails.username}
+      />
 
       <label>Password* :</label>
-      <input type="text" value={username} />
+      <input
+        type="text"
+        onChange={(e) => handleTextFieldChange(e)}
+        value={basicDetails.username}
+      />
 
       <label>Confirm Password* :</label>
-      <input type="text" value={username} />
+      <input
+        type="text"
+        onChange={(e) => handleTextFieldChange(e)}
+        value={basicDetails.username}
+      />
 
       <label>Mobile No.* :</label>
-      <input type="text" value={username} />
+      <input
+        type="text"
+        onChange={(e) => handleTextFieldChange(e)}
+        value={basicDetails.username}
+      />
     </div>
   );
 };
